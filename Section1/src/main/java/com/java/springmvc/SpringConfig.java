62160f917bb939f80a7d9cc354d2639da3166948
Tien Trinh <tientrinhjava@gmail.com>
2021-06-07 09:38:27 +0700

    section1

--- Section1/src/main/java/com/java/springmvc/SpringConfig.java
+++ Section1/src/main/java/com/java/springmvc/SpringConfig.java
@@ -1,10 +1,13 @@
 package com.java.springmvc;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
@@ -20,6 +23,19 @@ public class SpringConfig extends WebMvcConfigurerAdapter {
 		viewResolver.setPrefix("/WEB-INF/views/");
 		viewResolver.setSuffix(".jsp");
 		return viewResolver;
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/static/**").addResourceLocations("/resources/");
+	}
+	
+	@Bean
+	public MessageSource messageSource() {
+		ReloadableResourceBundleMessageSource bundleMessageSource = new ReloadableResourceBundleMessageSource();
+		bundleMessageSource.setBasename("classpath:messages");
+		bundleMessageSource.setDefaultEncoding("utf-8");
+		return bundleMessageSource;
 	}
 }
 
