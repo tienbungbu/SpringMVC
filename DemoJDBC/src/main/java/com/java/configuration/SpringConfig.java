c0104ebe98d4426c00538703d8a34fd4540681f0
TienBungBu <tientrinhjava@gmail.com>
2021-06-14 10:46:47 +0700

    update Demo Form JDBC + Validator + db.properties

--- DemoJDBC/src/main/java/com/java/configuration/SpringConfig.java
+++ DemoJDBC/src/main/java/com/java/configuration/SpringConfig.java
@@ -2,11 +2,15 @@ package com.java.configuration;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.multipart.MultipartResolver;
@@ -23,7 +27,9 @@ import com.java.validator.UserValidator;
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.java")
+@PropertySource(value = "classpath:db.properties")
 public class SpringConfig extends WebMvcConfigurerAdapter {
+	
 	@Bean
 	public ViewResolver viewResolver() {
 		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
@@ -58,13 +64,21 @@ public class SpringConfig extends WebMvcConfigurerAdapter {
 			return commonsMultipartResolver;
 	}
 	
+	@Autowired
+	Environment env;
+	
+	@Bean //JDBC by .properties
+	public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
+	
 	@Bean
 	public DataSource dataSource() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-		dataSource.setUrl("jdbc:mysql://localhost:3306/BAN_HANG_SPRING");
-		dataSource.setUsername("root");
-		dataSource.setPassword("");
+		dataSource.setDriverClassName(env.getProperty("mysql.driver"));
+		dataSource.setUrl(env.getProperty("mysql.url"));
+		dataSource.setUsername(env.getProperty("mysql.username"));
+		dataSource.setPassword(env.getProperty("mysql.password"));
 		return dataSource;
 	}
 	
